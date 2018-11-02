c5956ed072e4dee5f61ed1b2d20178e42d1644c0
Abdurrahman Al-Omari <abdurrahman.alomari@gmail.com>
2018-11-01 17:01:56 -0700

    converted java to kotlin and started working on edit/delete chore

--- app/src/main/java/com/dirtydish/app/dirtydish/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.dirtydish.app.dirtydish;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onClick(View view, int position, boolean isLongClick);
-}
--- app/src/main/java/com/dirtydish/app/dirtydish/modifyChore.kt
+++ app/src/main/java/com/dirtydish/app/dirtydish/modifyChore.kt
@@ -3,10 +3,12 @@ package com.dirtydish.app.dirtydish
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_modify_chore.*
+import kotlinx.android.synthetic.main.nav_header_main_menu.*
 
-class modify_chore : AppCompatActivity() {
+class ModifyChore : AppCompatActivity() {
 
     private lateinit var db: FirebaseDatabase
     private lateinit var choreRef: DatabaseReference
@@ -19,21 +21,24 @@ class modify_chore : AppCompatActivity() {
 
         db = FirebaseDatabase.getInstance()
         choreRef = db.getReference("chores")
-        choreQuery = choreRef.child("chores").orderByChild("name")
+
+        choreQuery = choreRef.child("chores").orderByChild("id").equalTo(intent.getStringExtra("id"))
+
+        var tv1:TextView = findViewById(R.id.modify_chore_text)
+        tv1.text = intent.getStringExtra("id")
 
 
         btn_delete.setOnClickListener {
-            deleteChore()
+            deleteChore(intent.getStringExtra("id"))
             finish()
         }
         btn_submit.setOnClickListener {
-            editChore()
+            editChore(intent.getStringExtra("id"))
             finish()
         }
     }
-
-    private fun editChore() {
-        val key = choreRef.key
+//TODO Fix these
+    private fun editChore(key:String) {
         Log.d(tag, key)
         if (key != null) {
             val chore = Chore(name = modify_chore_text.text.toString(), id = key)
@@ -41,8 +46,16 @@ class modify_chore : AppCompatActivity() {
         }
     }
 
-    private fun deleteChore() {
+    private fun deleteChore(key:String) {
         val key = choreRef.key
         Log.d(tag, key)
+        /*choreQuery.addListenerForSingleValueEvent( new ValueEventListener(){
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (querySnap:DataSnapshot in dataSnapshot.children){
+                    querySnap.ref.removeValue();
+                }
+            }
+        });*/
     }
+
 }
