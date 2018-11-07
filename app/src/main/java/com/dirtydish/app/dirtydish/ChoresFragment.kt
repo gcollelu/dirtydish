f5f8cb00c7ff22258dff818de0e33983acdb874d
Alexander Kiryukhin <sasha130297@gmail.com>
2018-11-07 15:48:09 -0800

    -Fixed issue with button listeners not attaching

--- app/src/main/java/com/dirtydish/app/dirtydish/ChoresFragment.kt
+++ app/src/main/java/com/dirtydish/app/dirtydish/ChoresFragment.kt
@@ -5,29 +5,22 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.*
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_chore_home.*
 
 class ChoresFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.activity_chore_home,
                 container, false)
 
-        //For some reason it won't bind the click listener to the button
-        btnAddNewChore?.setOnClickListener {
-            val intent = Intent(activity, AddChoreActivity::class.java)
-            startActivity(intent)
-        }
+        val btnAdd = view.findViewById<Button>(R.id.btnAddNewChore)
+        val btnView = view.findViewById<Button>(R.id.btnViewChoreSchedule)
 
-        btnViewChoreSchedule?.setOnClickListener {
-            val intent = Intent(activity, ViewChoresActivity::class.java)
-            startActivity(intent)
-        }
+        btnAdd.setOnClickListener { startActivity(Intent(activity, AddChoreActivity::class.java)) }
+        btnView.setOnClickListener { startActivity(Intent(activity, ViewChoresActivity::class.java)) }
+
         return view
     }
-
 
 }
