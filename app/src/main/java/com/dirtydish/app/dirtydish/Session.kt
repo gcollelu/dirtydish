fbd9572d0918b571d0f75a5fc55870390cd27ba9
Alexander Kiryukhin <sasha130297@gmail.com>
2018-11-10 15:50:34 -0800

    -Session housemate and house fields are now automatically updated -Some session functionality is now async

--- app/src/main/java/com/dirtydish/app/dirtydish/Session.kt
+++ app/src/main/java/com/dirtydish/app/dirtydish/Session.kt
@@ -1,10 +1,9 @@
 package com.dirtydish.app.dirtydish
 
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
+import org.jetbrains.anko.doAsync
 
 /*
 *
@@ -17,49 +16,59 @@ object Session {
     var userHouse: House? = null
     var housemate: HouseMate? = null
 
+    private var userRef: DatabaseReference? = null
+    private var houseRef: DatabaseReference? = null
+    private val db = FirebaseDatabase.getInstance()
+
+    private object housemateListener : ValueEventListener {
+        override fun onCancelled(p0: DatabaseError) {}
+        override fun onDataChange(p0: DataSnapshot) {
+            housemate = p0.getValue<HouseMate>(HouseMate::class.java)
+            val hm = housemate
+            if (userHouse == null && hm != null && !hm.houseId.isEmpty()) {
+                getHouseInfo(hm.houseId)
+            }
+        }
+    }
+
+    private object houseListener : ValueEventListener {
+        override fun onCancelled(p0: DatabaseError) {}
+        override fun onDataChange(p0: DataSnapshot) {
+            userHouse = p0.getValue<House>(House::class.java)
+        }
+    }
+
     init {
         initInfo()
     }
 
     private fun initInfo() {
-        val curUser = FirebaseAuth.getInstance().currentUser
-        if (curUser != null){
-            val db = FirebaseDatabase.getInstance()
-            val hmref = db.getReference("housemates")
-            val userRef = hmref.child(curUser.uid)
-            userRef.addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onCancelled(p0: DatabaseError) {}
-                override fun onDataChange(p0: DataSnapshot) {
-                    housemate = p0.getValue<HouseMate>(HouseMate::class.java)
-                    val hm = housemate
-                    if (hm != null && !hm.houseId.isEmpty()) {
-                        getHouseInfo(hm.houseId)
-                    }
-                }
-            })
+        doAsync {
+            val curUser = FirebaseAuth.getInstance().currentUser
+            if (curUser != null) {
+                userRef = db.getReference("housemates").child(curUser.uid)
+                userRef?.addValueEventListener(housemateListener)
+            }
         }
     }
 
-    private fun getHouseInfo(houseId: String){
-        val db = FirebaseDatabase.getInstance()
-        val housesRef = db.getReference("houses")
-        housesRef.child(houseId).addListenerForSingleValueEvent(object: ValueEventListener{
-            override fun onCancelled(p0: DatabaseError) {}
-
-            override fun onDataChange(p0: DataSnapshot) {
-                userHouse = p0.getValue<House>(House::class.java)
-            }
-        })
+    private fun getHouseInfo(houseId: String) {
+        houseRef = db.getReference("houses").child(houseId)
+        houseRef?.addValueEventListener(houseListener)
     }
 
     // call this on logout
-    fun clear(){
-        housemate = null
-        userHouse = null
+    fun clear() {
+        doAsync {
+            housemate = null
+            userHouse = null
+            userRef?.removeEventListener(housemateListener)
+            houseRef?.removeEventListener(houseListener)
+        }
     }
 
     // call this on login
-    fun init(){
+    fun init() {
         initInfo()
     }
 
@@ -68,7 +77,7 @@ object Session {
     }
 
     fun hasHouse(): Boolean {
-      return userHouse != null
+        return userHouse != null
     }
 
 
