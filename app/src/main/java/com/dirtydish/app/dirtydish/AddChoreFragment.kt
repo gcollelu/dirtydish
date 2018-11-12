61ae85419f7056448066559f6617545e680e1b34
Giulio Colleluori <gcollelu@ucsc.edu>
2018-11-12 13:25:08 -0800

    Implemented pick image functionality for new chore

--- app/src/main/java/com/dirtydish/app/dirtydish/AddChoreFragment.kt
+++ app/src/main/java/com/dirtydish/app/dirtydish/AddChoreFragment.kt
@@ -1,7 +1,14 @@
 package com.dirtydish.app.dirtydish
 
+import android.Manifest
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,11 +18,15 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_add_chore.*
 
+
 class AddChoreFragment : Fragment() {
 
     private lateinit var db: FirebaseDatabase
     private lateinit var choreRef: DatabaseReference
     private val tag_local = "CHORE_ADD"
+    private val READ_REQUEST_CODE = 101
+    private var myContext: Context? = null
+    val PICK_IMAGE = 1
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -26,12 +37,30 @@ class AddChoreFragment : Fragment() {
         choreRef = db.getReference("chores")
 
         val btnDone = view.findViewById<Button>(R.id.btnDone)
+        val btnPickImage = view.findViewById<Button>(R.id.btnPickImage)
 
+        setupPermissions()
         btnDone.setOnClickListener {
             createChore()
         }
 
+        btnPickImage.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE)
+        }
+
         return view
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        this.myContext = context
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     private fun createChore() {
@@ -43,4 +72,21 @@ class AddChoreFragment : Fragment() {
             choreRef.child(key).setValue(chore)
         }
     }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(myContext!!,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission to record denied")
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(activity!!,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                READ_REQUEST_CODE)
+    }
+
 }
