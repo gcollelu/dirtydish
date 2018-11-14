1d0906a30348633fc92e82ff71933bc4bf130f7b
Giulio Colleluori <gcollelu@ucsc.edu>
2018-11-13 21:33:19 -0800

    Re-implemented image picked

--- app/src/main/java/com/dirtydish/app/dirtydish/AddChoreFragment.kt
+++ app/src/main/java/com/dirtydish/app/dirtydish/AddChoreFragment.kt
@@ -1,7 +1,13 @@
 package com.dirtydish.app.dirtydish
 
+import android.Manifest
 import android.app.DatePickerDialog
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.util.Log
@@ -26,6 +32,9 @@ class AddChoreFragment : Fragment() {
     var participantsList: MutableList<HouseMate> = mutableListOf<HouseMate>()
     var housematesArray: MutableList<HouseMate> = mutableListOf<HouseMate>()
     var choreArray: MutableList<Chore> = mutableListOf<Chore>()
+    private val READ_REQUEST_CODE = 101
+    private var myContext: Context? = null
+    val PICK_IMAGE = 1
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -76,12 +85,31 @@ class AddChoreFragment : Fragment() {
             Toast.makeText(activity, "Chore created.", Toast.LENGTH_SHORT).show()
         }
 
+        btnPickImage.setOnClickListener {
+            setupPermissions()
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE)
+        }
+
         val startDate: TextView = view.findViewById(R.id.startDate)
         val endDate: TextView = view.findViewById(R.id.endDate)
+
 
         makeCalendarField(startDate)
         makeCalendarField(endDate)
 
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        this.myContext = context
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Toast.makeText(activity, "Imaged selected.", Toast.LENGTH_SHORT).show()
     }
 
     private fun createChore() {
@@ -136,5 +164,20 @@ class AddChoreFragment : Fragment() {
                     calendar.get(Calendar.MONTH),
                     calendar.get(Calendar.DAY_OF_MONTH)).show()
         }
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(myContext!!,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission to record denied")
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(activity!!,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                READ_REQUEST_CODE)
     }
 }
--- app/src/main/java/com/dirtydish/app/dirtydish/EditChoreFragment.kt
+++ app/src/main/java/com/dirtydish/app/dirtydish/EditChoreFragment.kt
@@ -1,7 +1,13 @@
 package com.dirtydish.app.dirtydish
 
+import android.Manifest
 import android.app.DatePickerDialog
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.util.Log
@@ -24,6 +30,9 @@ class EditChoreFragment : Fragment() {
     var housematesArray: MutableList<HouseMate> = mutableListOf<HouseMate>()
     var choreArray: MutableList<Chore> = mutableListOf<Chore>()
     var chore: Chore? = null
+    private val READ_REQUEST_CODE = 101
+    private var myContext: Context? = null
+    val PICK_IMAGE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -107,6 +116,23 @@ class EditChoreFragment : Fragment() {
             Toast.makeText(activity, "Chore saved.", Toast.LENGTH_SHORT).show()
         }
 
+        btnPickImage.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE)
+        }
+
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        this.myContext = context
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Toast.makeText(activity, "Imaged selected.", Toast.LENGTH_SHORT).show()
     }
 
     private fun editChore(key: String) {
@@ -165,6 +191,21 @@ class EditChoreFragment : Fragment() {
                     calendar.get(Calendar.MONTH),
                     calendar.get(Calendar.DAY_OF_MONTH)).show()
         }
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(myContext!!,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(ContentValues.TAG, "Permission to record denied")
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(activity!!,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                READ_REQUEST_CODE)
     }
 
 }
